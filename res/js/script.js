1a3e80419c0c6ad321427d9d8c6974e6e789d594
Kerli <kerli.otti@gmail.com>
2019-10-13 00:16:35 +0300

    final

--- res/js/script.js
+++ res/js/script.js
@@ -1,17 +1,20 @@
 $(document).ready(function(){
-    let user=new User('Jane',
-        'Doe',
-        '17/01/1990',
-        'Faculty of Science and Technology',
-        4.5
-    );
+
     let courses=[
-        new Course('Machine Learning', 1, 99),
-        new Course('Business Data Analytics', 1, 91),
-        new Course('Programming Languages', 2, 95),
-        new Course('School Software', 2, 81)
+        new Course('Alige software development', 1, 82),
+        new Course('System modeling', 1, 85),
+        new Course('Objekt-oriented programming', 2, 99),
+        new Course('Estonian language Level A2', 2, 65)
     ];
+
+    let user=new User('John',
+        'Doe',
+        '11/10/1990',
+        'Sftware Engeneering',
+        2.75
+    );
     init(); //initialize dynamical population
+
     function init() {
         $(".controls button#profile-button.pill").click(function (event) {
             $("#profile-container").addClass('active');
@@ -19,16 +22,19 @@ $(document).ready(function(){
             $("#profile-button").addClass('active');
             $("#courses-button").removeClass('active');
         });
+
         $('.controls button#courses-button.pill').click(function (event) {
             $("#profile-container").removeClass("active");
             $("#courses-container").addClass("active");
             $("#courses-button").addClass('active');
             $("#profile-button").removeClass('active');
         });
+
         $('.content button#add-course-button.blue-button').click(function (event) {
             // alert("You clicked me!")
             $("#add-course").toggle();
         });
+
         $("button#courses tbody").empty();
         for (let i=0; i < courses.length; i++) {
             let tr=$("<tr></tr>");
@@ -42,6 +48,7 @@ $(document).ready(function(){
             tr.append(cGrade);
             $("button#courses tbody").append(tr);
         }
+
         $('button#save-course.green-button').click(function (event) {
             // alert("You clicked me!")
             let table = $("#courses").find('tbody');
@@ -56,12 +63,41 @@ $(document).ready(function(){
             table.append($('<td>').text(grade));
             refreshToDefault()
             courses.push(new Course(title, semester, grade));
+            $("#gpa strong").text(avgGPA());
         });
+
         function refreshToDefault() {
             $('.input').val('');
             $('button#add-course').hide();
         }
 
+        $("#name").text(user.firstname + " " + user.lastname);
+        $("#birthdate").text(user.birthdate);
+        $("#faculty").text(user.faculty);
+        $("#gpa strong").text(user.gpa);
+
+        function avgGPA() {
+            let points = 0;
+            for (let i = 0; i < courses.length; i++) {
+                let grade = courses[i].grade;
+                if (grade > 90) {
+                    points += 4;
+                }
+                else if (grade > 80) {
+                    points += 3;
+                }
+                else if (grade > 70) {
+                    points += 2;
+                }
+                else if (grade > 60) {
+                    points += 1;
+                }
+                else if (grade > 50) {
+                    points += 0.5;
+                }
+            }
+            return points/courses.length;
+        }
     }
 });
 
