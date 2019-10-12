5c028776c498da8c4e6e0ea19cd832ee3a5ea4e1
Kerli <kerli.otti@gmail.com>
2019-10-12 22:33:36 +0300

    save button, adding, rows, input info to table

--- res/js/script.js
+++ res/js/script.js
@@ -1,11 +1,11 @@
 $(document).ready(function(){
-    let user = new User('Jane',
+    let user=new User('Jane',
         'Doe',
         '17/01/1990',
         'Faculty of Science and Technology',
         4.5
     );
-    let courses = [
+    let courses=[
         new Course('Machine Learning', 1, 99),
         new Course('Business Data Analytics', 1, 91),
         new Course('Programming Languages', 2, 95),
@@ -29,6 +29,39 @@ $(document).ready(function(){
             // alert("You clicked me!")
             $("#add-course").toggle();
         });
+        $("button#courses tbody").empty();
+        for (let i=0; i < courses.length; i++) {
+            let tr=$("<tr></tr>");
+            let rNo=$('<td></td>').text(i + 1);
+            let cName=$('<td></td>').text(courses[i].title);
+            let cSemester=$('<td></td>').text(courses[i].semester);
+            let cGrade=$('<td></td>').text(courses[i].grade);
+            tr.append(rNo);
+            tr.append(cName);
+            tr.append(cSemester);
+            tr.append(cGrade);
+            $("button#courses tbody").append(tr);
+        }
+        $('button#save-course.green-button').click(function (event) {
+            // alert("You clicked me!")
+            let table = $("#courses").find('tbody');
+            let nr = courses.length;
+            let title = $('#title').val();
+            let semester = $('#semester').val();
+            let grade = $('#grade').val();
+            table.append($('<tr>'));
+            table.append($('<td>').text(nr+1));
+            table.append($('<td>').text(title));
+            table.append($('<td>').text(semester));
+            table.append($('<td>').text(grade));
+            refreshToDefault()
+            courses.push(new Course(title, semester, grade));
+        });
+        function refreshToDefault() {
+            $('.input').val('');
+            $('button#add-course').hide();
+        }
+
     }
 });
 
