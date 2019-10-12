bf051881ef3bd6ba5e08bfec927f684fc9233ad4
Kerli <kerli.otti@gmail.com>
2019-10-12 17:31:21 +0300

    classes

---
+++ res/js/Course.js
@@ -0,0 +1,8 @@
+class Course{
+    constructor(title, semester, grade) {
+        this.selected = [];
+        this.title = title;
+        this.semester = semester;
+        this.grade = grade;
+    }
+}
---
+++ res/js/User.js
@@ -0,0 +1,9 @@
+class User{
+    constructor(firstname, lastname, birthdate, faculty, gpa) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.birthdate = birthdate;
+        this.faculty = faculty;
+        this.gpa = gpa;
+    }
+}
